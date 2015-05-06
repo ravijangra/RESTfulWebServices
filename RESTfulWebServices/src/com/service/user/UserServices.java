fe02d428df609bb32a788a8de6b75ee98b27c4e5
ravijangra <ravijangra@gmail.com>
2015-05-06 10:52:51 +0530

    RESTfulWebServices

---
+++ RESTfulWebServices/src/com/service/user/UserServices.java
@@ -0,0 +1,26 @@
+package com.service.user;
+
+import javax.ws.rs.*;
+
+@Path("/user/service")
+public class UserServices {
+	
+	@PUT
+	@Path("/create")
+	public void addUser(){
+		System.out.println("Inside addUser method");
+	}
+	@GET
+	public void getUser(){
+		System.out.println("Inside getUser method");
+	}
+	@POST
+	public void updateUser(){
+		System.out.println("Inside updateUser method");
+	}
+	@DELETE
+	public void deleteUser(){
+		System.out.println("Inside deleteUser method");
+	}
+
+}
